4bfa19ad7412c14dc3437f92ac738e13ac0a1183
aaschaer <aaschaer@uchicago.edu>
2016-05-15 15:41:11 -0500

    updated wavtotext, need to test on non windows

--- Processing3DPrintedRecord/wavtotext.py
+++ Processing3DPrintedRecord/wavtotext.py
@@ -1,8 +1,7 @@
-##//txt to stl conversion - 3d printable record
-##//by Amanda Ghassaei
-##//Dec 2012
+##//converts sound files to txt for LP printing
+##//Original code was from Amanda Ghassaei:
 ##//http://www.instructables.com/id/3D-Printed-Record/
-##
+## 
 ##/*
 ## * This program is free software; you can redistribute it and/or modify
 ## * it under the terms of the GNU General Public License as published by
@@ -10,45 +9,63 @@
 ## * (at your option) any later version.
 ##*/
 
-
+import os
+import sys
 import wave
 import math
 import struct
+from pydub import AudioSegment
 
 bitDepth = 8#target bitDepth
 frate = 44100#target frame rate
 
-fileName = "your_file_name_here.wav"#file to be imported (change this)
 
-#read file and get data
-w = wave.open(fileName, 'r')
-numframes = w.getnframes()
+def main():
 
-frame = w.readframes(numframes)#w.getnframes()
+    fileName = sys.argv[1] #file to be imported
 
-frameInt = map(ord, list(frame))#turn into array
+    print fileName[-4:]
+    print fileName[:-4]
+    if fileName[-4:] == ".mp3": #convert mp3 files
 
-#separate left and right channels and merge bytes
-frameOneChannel = [0]*numframes#initialize list of one channel of wave
-for i in range(numframes):
-    frameOneChannel[i] = frameInt[4*i+1]*2**8+frameInt[4*i]#separate channels and store one channel in new list
-    if frameOneChannel[i] > 2**15:
-        frameOneChannel[i] = (frameOneChannel[i]-2**16)
-    elif frameOneChannel[i] == 2**15:
-        frameOneChannel[i] = 0
-    else:
-        frameOneChannel[i] = frameOneChannel[i]
+    	#current strategy is to re-save the file as .wav
+    	#change the file name, and proceede as normal
+    	#should probably make this less jank in the future
+    	path = os.getcwd()
+    	sound = AudioSegment.from_mp3(path + "\\" + fileName)
+    	sound.export(path + "\\" + fileName[:-4] + ".wav", format="wav")
+    	fileName = fileName[:-4] + ".wav" #once 
 
-#convert to string
-audioStr = ''
-for i in range(numframes):
-    audioStr += str(frameOneChannel[i])
-    audioStr += ","#separate elements with comma
+	#read file and get data
+	w = wave.open(fileName, 'r')
+	numframes = w.getnframes()
 
-fileName = fileName[:-3]#remove .wav extension
-text_file = open(fileName+"txt", "w")
-text_file.write("%s"%audioStr)
-text_file.close()
+	frame = w.readframes(numframes)#w.getnframes()
+	frameInt = map(ord, list(frame))#turn into array
 
+	#separate left and right channels and merge bytes
+	frameOneChannel = [0]*numframes#initialize list of one channel of wave
+	for i in range(numframes):
+	    frameOneChannel[i] = frameInt[4*i+1]*2**8+frameInt[4*i]#separate channels and store one channel in new list
+	    if frameOneChannel[i] > 2**15:
+	        frameOneChannel[i] = (frameOneChannel[i]-2**16)
+	    elif frameOneChannel[i] == 2**15:
+	        frameOneChannel[i] = 0
+	    else:
+	        frameOneChannel[i] = frameOneChannel[i]
+
+	#convert to string
+	audioStr = ''
+	for i in range(numframes):
+	    audioStr += str(frameOneChannel[i])
+	    audioStr += ","#separate elements with comma
+
+	fileName = fileName[:-3]#remove .wav extension
+	text_file = open(fileName+"txt", "w")
+	text_file.write("%s"%audioStr)
+	text_file.close()
+
+if __name__ == "__main__":
+    main()
 
 
